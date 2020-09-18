db0bf18b9f1aadcc291108ac1621e2a7118847b9
Aude Richon <auderichon@gmail.com>
2020-09-18 10:24:26 +0200

    display list of movies and filtered movies

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <div class="container">
+    <div className="container">
       <Home />
     </div>
   );
--- src/components/movieList.jsx
+++ src/components/movieList.jsx
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 // import { Link } from "react-router-dom";
-
+import movies from "./../movies.json";
 import "./../styles/movieList.css";
 import "bulma/css/bulma.css";
 
-const movieList = ({ elem }) => {
-  return (
-    <div class="tile is-child box">
-      <p class="title">Two</p>
-    </div>
-  );
-};
+let moviesList = JSON.parse(JSON.stringify(movies.results));
+
+class movieList extends Component {
+  handleClick = (event) => {
+    let value = event.target.value;
+    this.props.handleTitleClick(value);
+  };
+
+  render() {
+    const copy = [...moviesList];
+    const filteredMovies = copy.filter((movie) =>
+      movie.title.toLowerCase().includes(this.props.searchValue.toLowerCase())
+    );
+
+    return (
+      <div className="tile is-child box">
+        <ul>
+          {filteredMovies.map((movie) => (
+            <li
+              className="hovered"
+              id={this.props.movieId === movie.id ? "highlighted" : ""}
+              onClick={this.handleClick}
+              key={movie.id}
+              value={movie.id}
+            >
+              {movie.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default movieList;
